78d92926b0cb535d986e8f9031cd7c4ff9d3c172
OwenYeah <68225896+OwenYeah@users.noreply.github.com>
2021-11-11 14:37:05 +0800

    Update settings.ts
    change to optional interfaces

--- src/html-flip-book/settings.ts
+++ src/html-flip-book/settings.ts
@@ -10,43 +10,43 @@ export interface IFlipSetting {
     width: number;
     height: number;
 
-    minWidth: number;
-    maxWidth: number;
-    minHeight: number;
-    maxHeight: number;
+    minWidth?: number;
+    maxWidth?: number;
+    minHeight?: number;
+    maxHeight?: number;
 
     /** Draw shadows or not when page flipping */
-    drawShadow: boolean;
+    drawShadow?: boolean;
     /** Flipping animation time */
-    flippingTime: number;
+    flippingTime?: number;
 
     /** Enable switching to portrait mode */
-    usePortrait: boolean;
+    usePortrait?: boolean;
     /** Initial value to z-index */
-    startZIndex: number;
+    startZIndex?: number;
     /** If this value is true, the parent element will be equal to the size of the book */
-    autoSize: boolean;
+    autoSize?: boolean;
     /** Shadow intensity (1: max intensity, 0: hidden shadows) */
-    maxShadowOpacity: number;
+    maxShadowOpacity?: number;
 
     /** If this value is true, the first and the last pages will be marked as hard and will be shown in single page mode */
-    showCover: boolean;
+    showCover?: boolean;
     /** Disable content scrolling when touching a book on mobile devices */
-    mobileScrollSupport: boolean;
+    mobileScrollSupport?: boolean;
 
     /** Set the forward event of clicking on child elements (buttons, links) */
-    clickEventForward: boolean;
+    clickEventForward?: boolean;
 
     /** Using mouse and touch events to page flipping */
-    useMouseEvents: boolean;
+    useMouseEvents?: boolean;
 
-    swipeDistance: number;
+    swipeDistance?: number;
 
     /** if this value is true, fold the corners of the book when the mouse pointer is over them. */
-    showPageCorners: boolean;
+    showPageCorners?: boolean;
 
     /** if this value is true, flipping by clicking on the whole book will be locked. Only on corners */
-    disableFlipByClick: boolean;
+    disableFlipByClick?: boolean;
 }
 
 export interface IBookState {
